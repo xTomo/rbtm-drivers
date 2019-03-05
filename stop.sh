48ce8ae7fcaea62a5cfe5d804165d433e7b49c6e
Alexey Buzmakov <buzmakov@gmail.com>
2019-03-05 16:55:44 +0300

    add stop script

--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
-cd /home/robotom/workspace/xtomo/rbtm-experiment && docker-compose down
-cd /home/robotom/workspace/xtomo/rbtm-drivers && docker-compose down
+cd /home/robotom/workspace/xtomo/rbtm-experiment && docker-compose down --remove-orphans
+cd /home/robotom/workspace/xtomo/rbtm-drivers && docker-compose down --remove-orphans
